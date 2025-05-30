c7fa2ff1c1468357b96ba8bf1d761098147272ea
Gerald <geraldnyerere100@gmail.com>
2025-05-30 15:14:02 +0300

    personal reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,16 @@
+task = input("Enter task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("is it time bound(yes/no): ")
+
+match priority:
+    case "high":
+        reminder = f"Reminder: '{task}' is a high priority task."
+    case "medium":
+         reminder = f"Reminder: '{task}' is a MEDIUM priority task."
+    case "low":
+         reminder = f"Reminder: '{task}' is a LOW priority task."
+    case "_":
+        reminder = f"Reminder: '{task}' is a UNKNOWN priority task."
+if time_bound == "yes":
+    reminder += " This task requires immediate attention!"
+print(reminder)
