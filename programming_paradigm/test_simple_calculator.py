b10839a4ef776f2adb640d1e21e76d4bba12f5da
Gerald <geraldnyerere100@gmail.com>
2025-06-13 18:03:24 +0300

    a script for test a simple calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,35 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-2, -3), -5)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5.0)
+        self.assertEqual(self.calc.divide(-6, 3), -2.0)
+        self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333333333, places=6)
+
+    def test_division_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
